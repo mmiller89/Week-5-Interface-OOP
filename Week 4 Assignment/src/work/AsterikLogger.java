8725313838b149cbb3561dc852bff2d4877e5cba
Michael Miller <86584725+mmiller89@users.noreply.github.com>
2022-06-01 18:47:40 -0700

    Update AsterikLogger.java

--- Week 4 Assignment/src/work/AsterikLogger.java
+++ Week 4 Assignment/src/work/AsterikLogger.java
@@ -12,12 +12,12 @@ public class AsterikLogger implements Logger {
 	public void Error(String error) {
 		//According to the word document instructions, I counted 12 total spaces in the middle line
 		//with 6 asteriks (3 on each side) and so I would conclude that the top and bottom row have
-		//n + n/2 asteriks length is n is equal to the total length of the entered message and each side of
+		//n + n/2 asteriks length if n is equal to the total length of the entered message and each side of
 		//the middle row would have n/4 asteriks.
 		
-		int length = error.length() + 7;
-		int reducerTopBottom = length + Math.floorDiv(length, 2) - 1;
-		
+		int length = error.length() + 7; //There are seven characters in "Error: " (line 36)
+		int reducerTopBottom = length + length/2 - 1;
+		int reducerMid = length/4 - 1;
 		
 		
 		
@@ -26,25 +26,31 @@ public class AsterikLogger implements Logger {
 			reducerTopBottom --;
 		}
 		
-		int reducerMid = Math.floorDiv(length, 4) - 1;
+		
 		System.out.println("*");
+		
 		while (reducerMid >= 0) {
 			System.out.print("*");
 			reducerMid --;
 		}
 		
 		System.out.print("Error: " + error);
-		reducerMid = Math.floorDiv(length, 4) - 2;
 		
-		while (reducerMid >=  0) {
+		//Reassign for asteriks following error message.
+		
+		reducerMid = length/4;
+		
+		while (reducerMid !=  0) {
 			System.out.print("*");
 			reducerMid --;
 		}
 		
-		reducerTopBottom = length + Math.floorDiv(length, 2) - 1;
+		//Reassing reducer for bottom layer
+		reducerTopBottom = length + length/2;
+		
 		System.out.println("*");
 		
-		while (reducerTopBottom >= 0) {
+		while (reducerTopBottom != 0) {
 			System.out.print("*");
 			reducerTopBottom --;
 		}
